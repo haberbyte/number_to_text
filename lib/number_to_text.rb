dc9320ad94d6e4623e8d77613d4b9dfb75e7eaf4
Jan Habermann <jan@habermann24.com>
2015-05-23 17:07:07 +0200

    Rename methods to be more ruby-like. Fix some failing tests

--- lib/number_to_text.rb
+++ lib/number_to_text.rb
@@ -1,35 +1,36 @@
 require 'number_to_text/version'
 
 module NumberToText
-  def self.NumberToText10(number)
-    number /= 10
+  def self.num_to_text_10(number)
+    number = number.to_i / 10
     case number
-    when 1 then return 'zehn'
-    when 2 then return 'zwanzig'
-    when 3 then return 'dreißig'
-    when 4 then return 'vierzig'
-    when 5 then return 'fünfzig'
-    when 6 then return 'sechzig'
-    when 7 then return 'siebzig'
-    when 8 then return 'achtzig'
-    when 9 then return 'neunzig'
-    else return 'FEHLER'
+    when 1 then 'zehn'
+    when 2 then 'zwanzig'
+    when 3 then 'dreißig'
+    when 4 then 'vierzig'
+    when 5 then 'fünfzig'
+    when 6 then 'sechzig'
+    when 7 then 'siebzig'
+    when 8 then 'achtzig'
+    when 9 then 'neunzig'
     end
   end
 
-  def self.NumberToText100(number, digits)
+  def self.num_to_text_100(number, digits)
     number = number % 100
 
     if number == 1
       case digits
-      when 0 then return 'eins'
-      when 1..3 then return 'ein'
-      when 6, 9, 12, 15 then
-        if number == 1 then return 'eine'
-        else return 'ein'
-        end
-      else
-        'FEHLER'
+      when 0
+        return 'eins'
+      when 1..3
+        return 'ein'
+      when 6, 9, 12, 15
+        if number == 1
+          return 'eine'
+        else
+         return 'ein'
+       end
       end
     end
 
@@ -53,63 +54,59 @@ module NumberToText
       when 17 then return 'siebzehn'
       when 18 then return 'achtzehn'
       when 19 then return 'neunzehn'
-      else return 'FEHLER'
       end
     elsif (number >= 20) && (number <= 99)
       if number % 10 == 0
-        return NumberToText10(number).to_s
+        return num_to_text_10(number).to_s
       else
-        return NumberToText100(number % 10, 1).to_s + 'und' + NumberToText10(number).to_s
+        return num_to_text_100(number % 10, 1).to_s + 'und' + num_to_text_10(number).to_s
       end
     end
-
-    ''
   end
 
-  def self.NumberToText1000(number, digits)
+  def self.num_to_text_1000(number, digits)
     if number / 100 == 0
-      return NumberToText100(number, digits).to_s
+      return num_to_text_100(number, digits).to_s
     else
-      return NumberToText100(number / 100, 2).to_s + 'hundert' + NumberToText100(number, digits).to_s
+      return num_to_text_100(number / 100, 2).to_s + 'hundert' + num_to_text_100(number, digits).to_s
     end
   end
 
-  def self.NumberToTextDigits(digits, mz)
+  def self.num_digits_to_text(digits, mz)
     if mz
       case digits
-        when 0 then return ''
-        when 3 then return 'tausend'
-        when 6 then return ' Millionen '
-        when 9 then return ' Milliarden '
-        when 12 then return ' Billionen '
-        when 15 then return ' Billiarden '
-        else return ''
+      when 0 then return ''
+      when 3 then return 'tausend'
+      when 6 then return ' Millionen '
+      when 9 then return ' Milliarden '
+      when 12 then return ' Billionen '
+      when 15 then return ' Billiarden '
       end
     else
       case digits
-        when 0 then return ''
-        when 3 then return 'tausend'
-        when 6 then return ' Million '
-        when 9 then return ' Milliarde '
-        when 12 then return ' Billion '
-        when 15 then return ' Billiarde '
-        else return ''
+      when 0 then return ''
+      when 3 then return 'tausend'
+      when 6 then return ' Million '
+      when 9 then return ' Milliarde '
+      when 12 then return ' Billion '
+      when 15 then return ' Billiarde '
       end
     end
   end
 
-  def self.convert(number)
+  def self.convert(num)
+    number = num.to_i
     digits = 0
     result = ''
 
     return 'null' if number == 0
 
     while number > 0
-      result = (number % 1000 > 0 ? NumberToText1000(number % 1000, digits).to_s + NumberToTextDigits(digits, number % 1000 > 1).to_s : '') + result
+      result = (number % 1000 > 0 ? num_to_text_1000(number % 1000, digits).to_s + num_digits_to_text(digits, number % 1000 > 1).to_s : '') + result
       number /= 1000
       digits += 3
     end
 
-    result
+    result.strip
   end
 end
